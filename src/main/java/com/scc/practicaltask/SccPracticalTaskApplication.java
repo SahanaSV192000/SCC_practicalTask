cd2de45cf18baa83cc33cf1ea35a74676b3d7645
Sahana <sasunand@cisco.com>
2026-08-27 08:52:25 +0530

    Initialize Spring Boot application

---
+++ src/main/java/com/scc/practicaltask/SccPracticalTaskApplication.java
@@ -0,0 +1,13 @@
+package com.scc.practicaltask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SccPracticalTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SccPracticalTaskApplication.class, args);
+	}
+
+}
