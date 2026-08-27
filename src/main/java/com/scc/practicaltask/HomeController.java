19451efb7d2aff70870fd5bd88124a0a134bb568
Sahana <sasunand@cisco.com>
2026-08-27 09:12:27 +0530

    Add devops topics library

--- src/main/java/com/scc/practicaltask/HomeController.java
+++ src/main/java/com/scc/practicaltask/HomeController.java
@@ -15,4 +15,9 @@ public class HomeController {
     public String plan() {
         return "plan";
     }
+
+    @GetMapping("/topics")
+    public String topics() {
+        return "topics";
+    }
 }
